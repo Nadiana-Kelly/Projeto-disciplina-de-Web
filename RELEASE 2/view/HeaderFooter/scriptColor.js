0f75589fcef2e70a25b4587e89af937c820213ef
Nadiana Kelly <82840116+Nadiana-Kelly@users.noreply.github.com>
2023-06-13 11:13:55 -0300

    Delete scriptColor.js

--- RELEASE 2/view/HeaderFooter/scriptColor.js
+++
@@ -1,8 +0,0 @@
-function alternarCSS() {
-    var cssFile = document.getElementById("cssFile");
-    var currentCSS = cssFile.getAttribute("href");
-    var newCSS = currentCSS === "style.css" ? "style1.css" : "style.css";
-    cssFile.setAttribute("href", newCSS);
-}
-
-document.getElementById("btnColor").addEventListener("click", alternarCSS);
